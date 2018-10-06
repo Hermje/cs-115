ba5677c98ae80c9851c683cf24fd95d863d37c67
Team1710CTO <team1710.cto@gmail.com>
2018-10-06 11:53:25 -0500

    added snack machine project

---
+++ projects/snack_machine/herman_jake_hw2.py
@@ -0,0 +1,49 @@
+# Name: Jake Herman
+# Section: C
+# Project: Snack Machine
+# Description:
+
+state = "A"
+total_inserted = 0
+snack_1 = 2
+snack_2 = 4
+snack_3 = 1
+snack_names = ["Chocolate Bar", "Doritos", "Granola Bar"]
+snack_choice = 0
+change = 0
+
+def main():
+    if state == "A":
+        command = input("input 'start' to begin: ")
+        if command == "start":
+            state = "B"
+            main()
+    elif state == "B":
+        total_inserted = float(input("Insert at least $2: "))
+        if total_inserted > 2:
+            change = total_inserted - 2
+            state = "C"
+        else:
+            main()
+    elif state == "C":
+        snack = int(input("choose a snack:\n 1: Chocolate Bar\n 2: Doritos\n 3: Granola Bar\n 4: refund"))
+        if snack < 5 and snack > 0:
+            snack_choice = snack
+            state = "D"
+            main()
+    elif state == "D":
+        if snack_choice < 4:
+            print("dispensing " + snack_names[snack_choice - 1])
+            state = "E"
+            main()
+        else:
+            print("refunding")
+            state = "A"
+            main()
+    elif state == "E":
+        print("your change is: " + change)
+        state = G
+    elif state == "G":
+        print("machine is off")
+
+main()
---
+++ projects/snack_machine/idea_state_machine.py
@@ -0,0 +1,17 @@
+
+
+class SuperState:
+    def __init__(name, states):
+        self.s = states
+        self.n = name
+
+    def add_state(state)
+        self.s.append(state)
+
+    def get_name():
+        return self.s.n
+
+class state()
+    def __init__(id, transition)
+        self.i = id
+        self.t = transition
